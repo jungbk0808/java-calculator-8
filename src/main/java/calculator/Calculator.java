ddbc9d34930cb4c2a4efd738367b5d39c24c51c7
jungbk0808 <jungbk0808@gmail.com>
2025-10-20 15:21:28 +0900

    feat(Calculator): 숫자 리스트를 입력으로 받아 숫자를 계산하는 클래스 구현

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package calculator;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class Calculator {
+    public BigDecimal add(List<BigDecimal> decimalList) {
+        BigDecimal result = BigDecimal.ZERO;
+        for (BigDecimal decimal : decimalList) {
+            result = result.add(decimal);
+        }
+        return result;
+    }
+}
