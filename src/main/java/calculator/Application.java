42e88adfd6b504fdd618ce4ce4568ca9a8ad2915
jungbk0808 <jungbk0808@gmail.com>
2025-10-20 15:38:07 +0900

    feat(Application): main 함수 작성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String line = Console.readLine();
+
+        List<BigDecimal> decimalList = DecimalParser.parse(line);
+        BigDecimal result = Calculator.add(decimalList);
+        System.out.println("결과 : " + result);
+
+        Console.close();
     }
 }
